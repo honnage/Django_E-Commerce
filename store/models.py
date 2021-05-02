78a56f3f617dee3c1146d813eb95e425a4a1bb38
unknown <hon_da-1998@hotmail.com>
2021-05-02 20:07:53 +0700

    Create Models Cart and CartItem

--- store/models.py
+++ store/models.py
@@ -40,3 +40,30 @@ class Product(models.Model):
 
     def get_url(self):
         return reverse('productDetails',args=[self.category.slug, self.slug])
+
+
+class Cart(models.Model):
+    cart_id = models.CharField(max_length=255, blank=True)
+    data_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.cart_id
+
+    class Meta:
+        db_table = 'cart'
+        ordering = ('data_added',)
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    available = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'cartItem'
+       
+    def sub_total(self):
+        return self.product.price + self.quantity
+
+    def __str__(self):
+        return self.cart
