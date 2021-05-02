f0c72c8567f4ee8603e22a5059d863c4bf189b31
unknown <hon_da-1998@hotmail.com>
2021-05-02 19:29:33 +0700

    Conma Price

--- djdngo_workshop/settings.py
+++ djdngo_workshop/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store'
+    'store',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
