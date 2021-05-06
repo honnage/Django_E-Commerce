43763217fc7b90942d8f4b5325a36f57639b481d
unknown <hon_da-1998@hotmail.com>
2021-05-06 23:59:28 +0700

    Create Models Order And OrderItem

--- store/models.py
+++ store/models.py
@@ -71,3 +71,34 @@ class CartItem(models.Model):
 
     def __str__(self):
         return self.product.name
+
+class Order(models.Model):
+    name = models.CharField(max_length=255, blank=True)
+    address = models.CharField(max_length=255, blank=True)
+    city = models.CharField(max_length=255, blank=True)
+    postcode = models.CharField(max_length=255, blank=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    email = models.EmailField(max_length=255, blank=True)
+    token = models.CharField(max_length=255, blank=True)
+
+    class Meta:
+        db_table = 'order'
+   
+    def __str__(self):
+        return str(self.id)
+
+
+class OrderItem(models.Model):
+    Product = models.CharField(max_length=255, blank=True)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'orderItem'
+    
+    def sub_total(self):
+        return self.quantity * self.price
+    
+    def __str__(self):
+        return self.product
