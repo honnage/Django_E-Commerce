ac6b33cc3339e59d671d6fa3589a99b3f2df1c54
unknown <hon_da-1998@hotmail.com>
2021-05-06 23:03:38 +0700

    Function ProductAdmin

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from store.models import Category,Product,Cart,CartItem
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','price','stock','created','update']
+    list_editable = ['price','stock']
+    list_per_page = 10
 
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Cart)
 admin.site.register(CartItem)
